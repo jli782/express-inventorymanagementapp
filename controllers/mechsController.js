752a607d5568eff988b49bbf4134f7f5a210b28a
Jeffrey Li <jli588@myseneca.ca>
2024-06-27 17:44:42 -0400

    added GET and POST controller and view for mech_form to create a mech

--- controllers/mechsController.js
+++ controllers/mechsController.js
@@ -5,6 +5,7 @@ const Category = require("../models/category");
 const Client = require("../models/client");
 const Manufacturer = require("../models/manufacturer");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   // res.send(`NOT IMPLEMENTED: Site Home Page`);
@@ -49,11 +50,119 @@ exports.mechs_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.mechs_create_GET = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Mechs create GET`);
+  res.render("mechs_form", {
+    title: "Create Mech",
+    errors: undefined,
+    mechs: undefined,
+  });
 });
-exports.mechs_create_POST = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Mechs create POST`);
-});
+exports.mechs_create_POST = [
+  body(`name`, `Battlemech name is empty.`)
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric(["en-US"], { ignore: " _-" })
+    .withMessage(`Battlemech name has non-alphanumeric characters.`),
+  body(`model`, `Model is empty.`)
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric(["en-US"], { ignore: " _-" })
+    .withMessage(`Battlemech model has non-alphanumeric characters.`),
+  body(`era`, `Era is empty.`)
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric(["en-US"], { ignore: " _-" })
+    .withMessage(`Era has non-alphanumeric characters.`),
+  body(`imageURL`)
+    .trim()
+    .isLength({ min: 1 })
+    .isURL()
+    .withMessage(`ImageURL is invalid.`),
+  body("tech", "Tech must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body(`equipment`)
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage(`Armaments is empty.`),
+  body(`description`)
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Description must be provided for the manufacturer."),
+  body(`weight`)
+    .trim()
+    .ltrim(`0`)
+    .isInt({
+      gt: -1,
+      lt: 999,
+    })
+    .withMessage(`Mech Weight is not a positive integer or out of bounds.`),
+  body(`price`)
+    .trim()
+    .ltrim(`0`)
+    .isInt({
+      gt: -1,
+    })
+    .withMessage(`Mech Weight is not a positive integer.`),
+  body(`battle_value`)
+    .trim()
+    .ltrim(`0`)
+    .isInt({
+      gt: -1,
+    })
+    .withMessage(`Battle Value is not a positive integer.`),
+  asyncHandler(async (req, res, next) => {
+    const err = validationResult(req);
+
+    // get the category property based on weight from form in req.body.weight
+    console.log(`req.body.weight - ${req.body.weight}`);
+    const mechCategory = await Category.findOne()
+      .where("weightMin")
+      .lte(req.body.weight)
+      .where("weightMax")
+      .gte(req.body.weight)
+      .exec();
+
+    console.log(`mechCategory - `, mechCategory);
+    err.array().map((e) => console.log(e));
+
+    const mech = new Mechs({
+      name: req.body.name,
+      model: req.body.model,
+      era: req.body.era,
+      tech: req.body.tech,
+      description: req.body.description,
+      weight: req.body.weight,
+      price: req.body.price,
+      battle_value: req.body.battle_value,
+      equipment: req.body.equipment.split(","),
+      imageURL: req.body.imageURL,
+      category: mechCategory,
+    });
+
+    if (!err.isEmpty()) {
+      res.render("mechs_form", {
+        title: `Create Mech`,
+        errors: err.array(),
+        mechs: mech,
+      });
+    } else {
+      const mechExists = await Mechs.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+
+      if (mechExists) {
+        res.redirect(mechExists.url);
+      } else {
+        await mech.save();
+        res.redirect(mech.url);
+      }
+    }
+    next();
+  }),
+];
 
 exports.mechs_delete_GET = asyncHandler(async (req, res, next) => {
   res.send(`NOT IMPLEMENTED: Mechs delete GET`);
